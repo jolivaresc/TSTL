b6e642552c458980e83d6bd1592c60d85cab0fc9
Olivares <o.castillojl@gmail.com>
2018-01-09 20:16:12 -0600

    se añade visualización del grafo mediante tensorboard

---
+++ prototypes/Prototype.py
@@ -0,0 +1,225 @@
+
+# coding: utf-8
+
+# In[1]:
+
+
+#http://www.ritchieng.com/machine-learning/deep-learning/tensorflow/deep-neural-nets/
+import pandas as pd
+import numpy as np
+import tensorflow as tf
+
+
+# In[2]:
+
+
+tf.set_random_seed(42)
+
+
+# In[3]:
+
+
+pd.set_option('display.max_colwidth', -1)
+pd.set_option('precision', 18)
+
+
+# In[4]:
+
+
+es = pd.read_csv("../vectors/es.node2vec.embeddings", delimiter = " ", skiprows = 1, header = None)
+na = pd.read_csv("../vectors/na.node2vec.embeddings", delimiter = " ", skiprows = 1, header = None)
+
+
+# In[5]:
+
+
+es.head()
+
+
+# In[6]:
+
+
+na.head()
+
+
+# In[7]:
+
+
+es_vectores = np.array(es[:1900].loc[:, 1::]).astype(np.float32)
+na_vectores = np.array(na[:1900].loc[:, 1::]).astype(np.float32)
+
+
+# In[8]:
+
+
+es_vectores
+
+
+# In[9]:
+
+
+na_vectores
+
+
+# In[10]:
+
+
+es_palabras = es[0]
+na_palabras = na[0]
+
+
+# In[11]:
+
+
+print("shape es:",es_vectores.shape,"\nshape na:",na_vectores.shape)
+
+
+# # Hyperparameters
+
+# In[13]:
+
+
+# Hyperparameters
+LEARNING_RATE = 0.1
+EPOCHS = 10
+BATCH_SIZE = 100
+
+NODES_INPUT = es_vectores[0].size
+NODES_H1 = 128
+NODES_H2 = 128
+NODES_OUPUT = na_vectores[0].size
+INSTANCES = es_vectores.__len__()
+NUM_STEPS = 5000
+
+
+# In[14]:
+
+
+X = tf.placeholder(shape=[None,NODES_INPUT], dtype=tf.float32,name='input_es')
+y = tf.placeholder(shape=[None,NODES_OUPUT], dtype=tf.float32,name='target_na')
+print("X:",X.shape,"y:",y.shape)
+
+
+# In[15]:
+
+
+# Capas ocultas (weights & bias)
+hidden_layer1 = {"W1":tf.Variable(tf.truncated_normal([NODES_INPUT,NODES_H1],stddev=0.01),name = 'W1'),
+                 "b1":tf.constant(0.1,shape=[NODES_H1], name = 'b1')}
+
+hidden_layer2 = {"W2":tf.Variable(tf.truncated_normal([NODES_H1,NODES_H2],stddev=0.01),name = 'W2'),
+                 "b2":tf.constant(0.1,shape=[NODES_H2], name = 'b2')}
+
+# Capa de salida
+output_layer = {"W_out":tf.Variable(tf.truncated_normal([NODES_H1,NODES_OUPUT],stddev=0.01),
+                name = 'W_output'),
+                "b_out":tf.constant(0.1,shape=[NODES_OUPUT], name = 'b_output')}
+
+
+tf.summary.histogram("weight_1", hidden_layer1["W1"])
+tf.summary.histogram("weight_2", hidden_layer2["W2"])
+tf.summary.histogram("weight_out", output_layer["W_out"])
+
+
+# In[16]:
+
+
+print(hidden_layer1)
+
+
+# In[17]:
+
+
+# Calcular la salida de la 1er capa oculta
+# h(x) = x*w + bias
+hidden_layer1_output = tf.add(tf.matmul(es_vectores, hidden_layer1["W1"]), hidden_layer1["b1"])
+# Función de activación usando ReLU
+hidden_layer1_output = tf.nn.relu(hidden_layer1_output,name="h1Activation")
+
+
+# In[18]:
+
+
+# Calcular la salida de la 2da capa oculta
+# h(x) = x*w + bias
+hidden_layer2_output = tf.add(tf.matmul(hidden_layer1_output, hidden_layer2["W2"]), hidden_layer2["b2"])
+# Función de activación usando ReLU
+hidden_layer2_output = tf.nn.relu(hidden_layer2_output,name="h1Activation")
+
+
+# In[19]:
+
+
+# calcular la salida la NN
+output = tf.add(tf.matmul(hidden_layer2_output, output_layer["W_out"]),output_layer["b_out"])
+# Función de activación usando softmax
+nah_predicted = tf.nn.softmax(output,name="outActivation")
+print(nah_predicted.shape,y.shape)
+
+
+# In[20]:
+
+
+# Función de error (Mean Square Error)
+#loss = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(labels = y, logits = nah_predicted))
+loss = tf.reduce_mean(tf.squared_difference(nah_predicted,y),name="loss_f")
+tf.summary.scalar("cost",loss)
+#loss = tf.reduce_sum((nah_predicted - y) ** 2)
+
+
+# In[21]:
+
+
+# optimiser, 
+optimiser = tf.train.GradientDescentOptimizer(learning_rate = LEARNING_RATE,name="GradientDescent").minimize(loss,name="loss")
+
+
+# In[22]:
+
+
+#Session
+sess = tf.Session()
+# Initialize variables
+summaryMerged = tf.summary.merge_all()
+writer = tf.summary.FileWriter("./logs/NN",sess.graph)
+init = tf.global_variables_initializer()
+sess.run(init)
+
+# In[23]:
+
+
+tmp_hidden_layer1,tmp_hidden_layer2 = sess.run(hidden_layer1),sess.run(hidden_layer2)
+
+
+# In[ ]:
+
+
+for step in range(3000):
+    '''
+    offset = (step * BATCH_SIZE) % (es_vectores.shape[0] - BATCH_SIZE)
+    batch_data = es_vectores[offset:(offset + BATCH_SIZE), :]
+    batch_target = na_vectores[offset:(offset + BATCH_SIZE), :]
+    '''
+    _loss,_, sumOut = sess.run([loss,optimiser, summaryMerged], feed_dict={
+                        X: es_vectores, y: na_vectores})
+    writer.add_summary(_loss,step)
+
+
+# In[24]:
+
+
+print(sess.run(hidden_layer1))
+
+
+# In[25]:
+
+
+
+tmp_hidden_layer1
+
+
+# In[26]:
+
+
+sess.run(hidden_layer2)
+
--- prototypes/hyp.py
+++ prototypes/hyp.py
@@ -16,62 +16,75 @@ INSTANCES = 1000
 
 # es - datos de entrada;
 # na - datos objetivos
-esp = np.random.uniform(0,15,size=[INSTANCES,NODES_INPUT]).astype(np.float32)
-nah = np.random.uniform(0,3,size=[INSTANCES,NODES_OUPUT]).astype(np.float32)
-print(esp.shape,nah.shape)
+esp = np.random.uniform(
+    0, 15, size=[INSTANCES, NODES_INPUT]).astype(np.float32)
+nah = np.random.uniform(0, 3, size=[INSTANCES, NODES_OUPUT]).astype(np.float32)
+print(esp.shape, nah.shape)
 
 # entrada: X -> español
 # salida: y -> náhuatl
-X = tf.placeholder(shape=[None,NODES_INPUT], dtype=tf.float32,name='X')
-y = tf.placeholder(shape=[None,NODES_OUPUT], dtype=tf.float32,name='y')
-print(X.shape,y.shape)
+X = tf.placeholder(shape=[None, NODES_INPUT], dtype=tf.float32, name='X')
+y = tf.placeholder(shape=[None, NODES_OUPUT], dtype=tf.float32, name='y')
+print(X.shape, y.shape)
 
 
 # Capa oculta (weights & bias)
-layer1 = {"W1":tf.Variable(tf.random_normal([NODES_INPUT,NODES_H1],stddev=0.01),name = 'W1'),
-          "b1":tf.Variable(tf.random_normal([NODES_H1]), name = 'b1')}
+layer1 = {"W1": tf.Variable(tf.random_normal([NODES_INPUT, NODES_H1], stddev=0.01), name='W1'),
+          "b1": tf.Variable(tf.random_normal([NODES_H1]), name='b1')}
 
-layer2 = {"W2":tf.Variable(tf.random_normal([NODES_H1,NODES_H2],stddev=0.01),name = 'W2'),
-          "b2":tf.Variable(tf.random_normal([NODES_H2]), name = 'b2')}
+layer2 = {"W2": tf.Variable(tf.random_normal([NODES_H1, NODES_H2], stddev=0.01), name='W2'),
+          "b2": tf.Variable(tf.random_normal([NODES_H2]), name='b2')}
 
 # Capa de salida
-output_layer = {"W2":tf.Variable(tf.random_normal([NODES_H2,NODES_OUPUT],stddev=0.01),name = 'W_output'),
-                "b2":tf.Variable(tf.random_normal([NODES_OUPUT]), name = 'b_output')}
+output_layer = {"W2": tf.Variable(tf.random_normal([NODES_H2, NODES_OUPUT], stddev=0.01), name='W_output'),
+                "b2": tf.Variable(tf.random_normal([NODES_OUPUT]), name='b_output')}
 
 print(layer1["W1"].shape)
 # Calcular la salida de la 1er capa oculta
 # h(x) = x*w + bias
-hidden_layer1_output = tf.add(tf.matmul(esp, layer1["W1"]), layer1["b1"],name="hidden1")
+hidden_layer1_output = tf.add(
+    tf.matmul(esp, layer1["W1"]), layer1["b1"], name="hidden1")
 # Función de activación usando ReLU
-hidden_layer1_output = tf.nn.relu(hidden_layer1_output,name="actfhidden1")
+hidden_layer1_output = tf.nn.relu(hidden_layer1_output, name="actfhidden1")
 
 # Calcular la salida de la 2da capa oculta
 # h(x) = x*w + bias
-hidden_layer2_output = tf.add(tf.matmul(hidden_layer1_output, layer2["W2"]), layer2["b2"],name="hidden2")
+hidden_layer2_output = tf.add(
+    tf.matmul(hidden_layer1_output, layer2["W2"]), layer2["b2"], name="hidden2")
 # Función de activación usando ReLU
-hidden_layer2_output = tf.nn.relu(hidden_layer2_output,name="actfhidden2")
+hidden_layer2_output = tf.nn.relu(hidden_layer2_output, name="actfhidden2")
 
 
 # calcular la salida la NN
-output = tf.add(tf.matmul(hidden_layer2_output, output_layer["W2"]),output_layer["b2"],name="output")
+output = tf.add(tf.matmul(hidden_layer2_output,
+                          output_layer["W2"]), output_layer["b2"], name="output")
 # Función de activación usando softmax
-nah_predicted = tf.nn.softmax(output,name="actfoutput")
+nah_predicted = tf.nn.softmax(output, name="actfoutput")
 
 # Función de error (Mean Square Error)
 #loss = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(labels = y,logits = nah_predicted))
-loss = tf.reduce_mean(tf.squared_difference(nah_predicted,y),name="loss_f")
+loss = tf.reduce_mean(tf.squared_difference(nah_predicted, y), name="loss_f")
 #loss = tf.reduce_sum((nah_predicted - y) ** 2)
 
-# optimiser, 
-optimiser = tf.train.GradientDescentOptimizer(learning_rate = LEARNING_RATE,name="GradientDescent").minimize(loss,name="loss")
+# optimiser,
+optimiser = tf.train.GradientDescentOptimizer(
+    learning_rate=LEARNING_RATE, name="GradientDescent").minimize(loss, name="loss")
 
 with tf.Session() as sess:
     sess.run(tf.global_variables_initializer())
 
-    writer = tf.summary.FileWriter("./logs/NN",sess.graph)
+    writer = tf.summary.FileWriter("./logs/NN", sess.graph)
     for _ in range(5000):
-        _, c = sess.run([optimiser,loss],feed_dict={X: esp, y: nah})
+        _, c = sess.run([optimiser, loss], feed_dict={X: esp, y: nah})
     print(c)
-    
 
-#tensorboard --logdir=logs
+
+'''
+tensorboard --logdir=logs
+def train_neural_network(x):
+    prediction=neural_network_model(x)
+    cost=tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits=prediction,labels=y,name=None))
+    optimizer=tf.train.AdamOptimizer().minimize(cost)
+Change the weights initialization to tf.truncated_normal(shape, stddev=0.1) and 
+bias to tf.constant(0.1, shape=shape). Got 98% accuracy. :D
+'''
