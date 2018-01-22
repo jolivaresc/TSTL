a227081aadb99348a5e03958c690db154e4f6568
jose <o.castillo@gmail.com>
2018-01-22 13:53:55 -0600

    se añade función p@k

--- prototypes/utils.py
+++ prototypes/utils.py
@@ -169,3 +169,44 @@ def get_closest_words_to(top_10, dataframe):
     """
 
     return [dataframe.iloc[index][0] for index, _ in top_10]
+
+
+#import numpy as np
+
+
+def apk(actual, predicted, k=10):
+    """
+    Computes the average precision at k.
+    This function computes the average prescision at k between two lists of
+    items.
+    Parameters
+    ----------
+    actual : list
+             A list of elements that are to be predicted (order doesn't matter)
+    predicted : list
+                A list of predicted elements (order does matter)
+    k : int, optional
+        The maximum number of predicted elements
+    Returns
+    -------
+    score : double
+            The average precision at k over the input lists
+            
+    https://github.com/benhamner/Metrics/blob/master/Python/ml_metrics/average_precision.py
+    """
+
+    if len(predicted) > k:
+        predicted = predicted[:k]
+
+    score = 0.0
+    num_hits = 0.0
+
+    for i,p in enumerate(predicted):
+        if str(p) in actual and str(p) not in predicted[:i]:
+            num_hits += 1.0
+            score += num_hits / (i+1.0)
+
+    if not actual:
+        return 0.0
+
+    return score / min(len(actual), k)
